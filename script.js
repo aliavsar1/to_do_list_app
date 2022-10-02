0603ff347e5d142138f9979ecb60b4b779f77ecb
Ali Avsar <avsarali2@gmail.com>
2022-10-02 13:50:33 +0300

    localstorage kullandım

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 "use strict";
+
+// Variables
 const myBtn = document.getElementById("myBtn");
 const pElement = document.getElementById("demo");
-
-let dailyThingsToDo = {};
 let i = 0;
+//check whether localstorage is being used and the number of to-do-list items
+if (localStorage.getItem(1) === null) {
+  pElement.innerHTML = "Listeniz boş. Yeni yapılacak iş ekleyin.";
+}
+
+//SHow the existing to do list
+showListBtn.addEventListener("click", function () {
+  //LOCAL STORAGE
+  for (i = 1; i <= 10; i++) {
+    let itemOfAim = localStorage.getItem(i);
+
+    let html = `<div class="aimItem">${i}. ${itemOfAim}</div>`;
+    pElement.insertAdjacentHTML("beforeend", html);
+  }
+});
+
+//ADD AN ITEM TO THE LIST
 
 myBtn.addEventListener("click", function () {
   i = i + 1;
@@ -15,10 +32,17 @@ myBtn.addEventListener("click", function () {
 
   function adder() {
     const aimItem = document.getElementById("aim").value;
-
+    localStorage.setItem(`${i}`, `${aimItem}`);
     return aimItem;
   }
+  //LOCAL STORAGE
+  localStorage.setItem(i, adder());
+  let itemOfAim = localStorage.getItem(i);
 
-  let html = `<div class="aimItem">${i}. ${adder()}</div>`;
+  let html = `<div class="aimItem">${i}. ${itemOfAim}</div>`;
   pElement.insertAdjacentHTML("beforeend", html);
+});
+//CLEAR LOCAL STORAGE
+clearBtn.addEventListener("click", function () {
+  localStorage.clear();
 });
