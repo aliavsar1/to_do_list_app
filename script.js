8dd15907d4a2fcb283dde7066b3047cfd401aed2
Ali Avsar <avsarali2@gmail.com>
2022-10-08 15:04:06 +0300

    managed dom and LS to display data

--- script.js
+++ script.js
@@ -2,73 +2,53 @@
 
 // Variable assignments
 const addButton = document.getElementById("addBtn");
+const removeButton = document.getElementById("removeBtn");
 const displayListButton = document.getElementById("displayListBtn");
 const clearAllButton = document.getElementById("clearAllBtn");
-const displaGreeting = document.getElementById("displayGreeting");
+const displayGreeting = document.getElementById("displayGreeting");
 const displayMessage = document.getElementById("displayMessage");
-const displayItems = document.getElementById("displayItems");
-
+let displayItems = document.getElementById("displayItems");
 const aimItem = document.getElementById("aim");
-let toDoListArray = [];
 let i = 0;
-const listItems = JSON.parse(localStorage.getItem("toDoListArray"));
+let toDoListArr = [];
+let listItems = JSON.parse(localStorage.getItem("toDoListArr"));
 
-//Date function
-function dateFunction(newDate) {
-  const dateObj = new Date();
-  const day = dateObj.getUTCDate();
-  const month = [
-    "Ocak",
-    "Şubat",
-    "Mart",
-    "Nisan",
-    "Mayıs",
-    "Haziran",
-    "Temmuz",
-    "Ağustos",
-    "Eylül",
-    "Ekim",
-    "Kasım",
-    "Aralık",
-  ][dateObj.getUTCMonth() + 1];
-  const daysOfTheWeek = [
-    "Pazar",
-    "Pazartesi",
-    "Salı",
-    "Çarşamba",
-    "Perşembe",
-    "Cuma",
-    "Cumartesi",
-  ][dateObj.getDay()];
-  const year = dateObj.getUTCFullYear();
-  return (newDate = `${"Bugün " + day + " " + month + " " + daysOfTheWeek}`);
-}
+// display the items
 
-//add items to the list
+//add items to the array
 addButton.addEventListener("click", function () {
-  if (i > 4) {
-    displayMessage.innerHTML = "5'ten fazla görev ekleyemezsiniz!";
-    return;
-  }
-  toDoListArray[i] = aimItem.value;
-  localStorage.setItem("toDoListArray", JSON.stringify(toDoListArray));
-  i = i + 1;
-});
+  if (listItems === null) {
+    toDoListArr[i] = aimItem.value;
+    localStorage.setItem("toDoListArr", JSON.stringify(toDoListArr));
+    listItems = JSON.parse(localStorage.getItem("toDoListArr"));
+    listItems.forEach(myFunction);
+    function myFunction(listItems, index, item) {
+      const html = `${
+        index + 1
+      }. ${item}.<button id="${item}" onclick="removerBtn()">Görev yapıldı</button><br>`;
+      document
+        .querySelector("#displayItems")
+        .insertAdjacentHTML("beforeend", html);
+    }
+  } else {
+    i = listItems.length + 1;
+    toDoListArr = listItems;
+    toDoListArr.push(aimItem.value);
+    toDoListArr = toDoListArr;
+    localStorage.setItem("toDoListArr", JSON.stringify(toDoListArr));
 
-//display items in the list
-displayListButton.addEventListener("click", function () {
-  listItems.forEach(myFunction);
-  function myFunction(index, item) {
-    displayItems.innerHTML += `${item + 1}. ${index}.<br>`;
+    displayItems.innerHTML = "";
+
+    listItems.forEach(myFunction);
+    function myFunction(index, item) {
+      const html = `${
+        item + 1
+      }. ${index}.<button id="${item}" onclick="removerBtn()">Görev yapıldı</button><br>`;
+      document
+        .querySelector("#displayItems")
+        .insertAdjacentHTML("beforeend", html);
+    }
   }
-});
-//remove items
-function remove() {
-  // const greetings = JSON.parse(localStorage.getItem("greetings"));
-  const filtered = listItems.filter((item) => item !== "Hello");
-  localStorage.setItem("greetings", JSON.stringify(filtered));
-}
-//remove all items
-clearAllButton.addEventListener("click", function () {
-  localStorage.clear();
+
+  i = i + 1;
 });
